88fea960a0ba8809d0ca94026f2b725047916591
Robert Borg <robert.borg@swsphn.com.au>
2026-02-23 16:46:49 +1100

    Reorganise into functions

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,24 +1,69 @@
-name = input("Enter your first name: ")
+def bmi_calculation(weight, height):
+    """
+    Calculate the Body Mass Index (BMI), rounded to 2 decimal places.
 
-weight = float(input("Enter your weight in kilograms (kg): "))
+    This function assumes:
+        - weight is provided in kilograms (kg)
+        - height is provided in centimetres (cm)
 
-height = float(input("Enter your height in centimetres (cm): "))
+    Args:
+        weight (float or int): The individual's weight in kilograms.
+        height (float or int): The individual's height in centimetres.
 
-BMI = round((weight/height/height * 10000), 2)
+    Returns:
+        float: The calculated BMI value rounded to two decimal places.
+    """
 
-print("Hello " + name + ",")
-print("Your BMI is " + str(BMI))
+    bmi = round((weight / height / height * 10000), 2)
+    return bmi
 
-if BMI>0:
-    if(BMI<18.5):
-        print("Underweight")
-    elif(BMI<=24.9):
-        print("Normal weight")
-    elif(BMI<=29.9):
-        print("Overweight")
-    elif(BMI<=34.9):
-        print("Obese")
-    elif(BMI<=39.9):
-        print("Serverely obese")
-    else:
-        print("Morbidly obese")
+
+def bmi_category_calculation(bmi):
+    """
+    Determine the BMI category based on a BMI value.
+
+    The classification is based on standard BMI ranges:
+        - < 18.5: Underweight
+        - 18.5–24.9: Normal weight
+        - 25.0–29.9: Overweight
+        - 30.0–34.9: Obese
+        - 35.0–39.9: Severely obese
+        - ≥ 40.0: Morbidly obese
+
+    Args:
+        bmi (float or int): The Body Mass Index value.
+
+    Returns:
+        str: The BMI category corresponding to the given BMI value.
+    """
+    if bmi > 0:
+        if bmi < 18.5:
+            bmi_category = "underweight"
+        elif bmi <= 24.9:
+            bmi_category = "normal weight"
+        elif bmi <= 29.9:
+            bmi_category = "overweight"
+        elif bmi <= 34.9:
+            bmi_category = "obese"
+        elif bmi <= 39.9:
+            bmi_category = "severely obese"
+        else:
+            bmi_category = "morbidly obese"
+
+    return bmi_category
+
+
+def main():
+    name = input("Enter your first name: ")
+    weight = float(input("Enter your weight in kilograms (kg): "))
+    height = float(input("Enter your height in centimetres (cm): "))
+    bmi = bmi_calculation(weight, height)
+    bmi_category = bmi_category_calculation(bmi)
+
+    print(f"\nHello {name}")
+    print(f"Your BMI is {bmi}")
+    print(f"This is categorised as {bmi_category}")
+
+
+if __name__ == "__main__":
+    main()
